58f392e0b2ff4630f7f16ba35fd5552a587d402c
sekthor <sysdroyed@gmail.com>
2021-08-04 21:23:11 +0200

    basic structure of all services

--- movie-info-service/src/main/java/ch/sekthor/movieinfoservice/models/Movie.java
+++ movie-info-service/src/main/java/ch/sekthor/movieinfoservice/models/Movie.java
@@ -1,2 +1,28 @@
-package ch.sekthor.movieinfoservice.models;public class Movie {
+package ch.sekthor.movieinfoservice.models;
+
+public class Movie {
+
+    private int movieId;
+    private String name;
+
+    public Movie(int movieId, String name) {
+        this.movieId = movieId;
+        this.name = name;
+    }
+
+    public int getMovieId() {
+        return movieId;
+    }
+
+    public void setMovieId(int movieId) {
+        this.movieId = movieId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
